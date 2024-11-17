c665a2a8873c2f8498e2ca298f79505007df3927
moinuddin3177 <moinuddin842@gmail.com>
2024-11-17 16:39:32 +0000

    HelloWorld using Render and CreateElement

---
+++ main.js
@@ -0,0 +1,22 @@
+const heading = React.createElement(
+    "h1",
+    { id: "heading", xyz: "abc" },
+    "Hello from React");
+
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child1" }, [
+        React.createElement("h1", {}, "I am parent child element from h1"),
+        React.createElement("h2", {}, "I am parent child element from h2")
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "I am parent child element from h1"),
+        React.createElement("h2", {}, "I am parent child element from h2")
+    ])]
+);
+
+console.log(parent);
+// root.render(heading);
+root.render(parent);
